063f76437a78991d15fc0562725a5fb361be7926
irsish <pancake6778@outlook.com>
2026-06-30 12:12:31 -0400

    Revert "Add files via upload"
    This reverts commit eb92a4cc7d822c61c0a3929388d5f928ee4d9f91.

--- routes/genomicFeatures.js
+++
@@ -1,8 +0,0 @@
-const express    = require('express')
-const controller = require('../controllers/genomicFeaturesController')
-
-const router = express.Router()
-
-router.get('/', controller.getFeatures)
-
-module.exports = router
